409e9dbca9cc8a7ce0d853bbe7a796ee4d0d84cc
ClarenceLiu5461 <frank1213450@gmail.com>
2023-03-15 17:25:40 +0800

    Dice image change finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject Dice;
+    public Transform Dice;
     public GameObject Player;
+    public Sprite DiceOne;
+    public Sprite DiceTwo;
+    public Sprite DiceThree;
+    public Sprite DiceFour;
+    public Sprite DiceFive;
+    public Sprite DiceSix;
     void Start()
     {
         
@@ -14,6 +21,27 @@ public class PlayerController : MonoBehaviour
     public void RollDice()
     {
         int Move = Random.Range(1, 7);
+        switch (Move)
+        {
+            case 1:
+                Dice.GetComponent<Image>().sprite = DiceOne;
+                break;
+            case 2:
+                Dice.GetComponent<Image>().sprite = DiceTwo;
+                break;
+            case 3:
+                Dice.GetComponent<Image>().sprite = DiceThree;
+                break;
+            case 4:
+                Dice.GetComponent<Image>().sprite = DiceFour;
+                break;
+            case 5:
+                Dice.GetComponent<Image>().sprite = DiceFive;
+                break;
+            case 6:
+                Dice.GetComponent<Image>().sprite = DiceSix;
+                break;
+        }
         Debug.Log(Move);
     }
 
