7b1771acfa00f8c589a3fef4eebc86470ef8c14c
ClarenceLiu5461 <frank1213450@gmail.com>
2023-04-05 02:42:11 +0800

    Add  overlook function finished

---
+++ Assets/Scripts/OverlookController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverlookController : MonoBehaviour
+{
+    public GameObject MainCamera;
+    public GameObject Camera2;
+    public Text btnText;
+    public GameObject Dice;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Overlook()
+    {
+        if (btnText.text == "¥þ´º")
+        {
+            MainCamera.SetActive(false);
+            Dice.SetActive(false);
+            Camera2.SetActive(true);
+            btnText.text = "³¡¤À";
+        }
+        else if (btnText.text == "³¡¤À")
+        {
+            Camera2.SetActive(false);
+            Dice.SetActive(true);
+            MainCamera.SetActive(true);
+            btnText.text = "¥þ´º";
+        }
+    }
+}
