24635078b4df92ff930da2ca03f0f3c1d912724f
ClarenceLiu5461 <frank1213450@gmail.com>
2023-05-02 19:58:46 +0800

    Get data from web

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,38 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
-using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 public class GameData : MonoBehaviour
 {
     public static int Past;
     public static int DiceNum;
 
-    void Start()
+    private async void Start()
     {
-        StartCoroutine(SendRequest());
+        // 發送 HTTP 請求並接收回傳的資料
+        string response = await SendHttpRequest();
+
+        // 解析 JSON 格式的回傳資料
+        Data[] dataArray = JsonConvert.DeserializeObject<Data[]>(response);
+        
+        //設定玩家位置和骰子數量
+        Data data = new Data();
+        Past = data.userlocation;
+        DiceNum = data.dice;
     }
-    //Get Past & DiceNum Variable
-    IEnumerator SendRequest()
+
+    private async Task<string> SendHttpRequest()
     {
-        string strUrl = "http://localhost/monopolyweb.php";
-
-        UnityWebRequest request = UnityWebRequest.Get(strUrl);
-        yield return request.SendWebRequest();
-        //Connection Error
-        if (request.result == UnityWebRequest.Result.ConnectionError)
+        using (HttpClient client = new HttpClient())
         {
-            Debug.Log(request.error);
-            yield break;
-        }
-        //Get DiceNum remain
-        string html = request.downloadHandler.text;
-        Debug.Log(html);
+            string phpUrl = "../from/php/gameuser.php"; // PHP 程式的網址
 
-        var received_data = Regex.Split(html, "</next>");
-        Debug.Log("DiceNum = " + html.ToString());
-        DiceNum = int.Parse(html);
+            // 發送 GET 請求並接收回應
+            HttpResponseMessage response = await client.GetAsync(phpUrl);
+
+            // 確保請求成功
+            response.EnsureSuccessStatusCode();
+
+            // 讀取回應內容
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // 返回回應內容
+            return responseBody;
+        }
+    }
+
+    // 定義一個類別來封裝回傳的資料結構
+    public class Data
+    {
+        public int userlocation { get; set; }
+        public int dice { get; set; }
     }
     //Be called when shut down the program 
     void OnDestory()
