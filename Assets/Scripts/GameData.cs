13da6fa25281f54a9d46d5cb49dd21d903068655
ClarenceLiu5461 <frank1213450@gmail.com>
2023-05-09 22:55:06 +0800

    Send string

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -10,17 +10,28 @@ public class GameData : MonoBehaviour
     public static int Past;
     public static int DiceNum;
 
-    void Awake()
+    IEnumerator Start()
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get("http://localhost:8002/form/php/gameuser.php"))
         {
-            string response = webRequest.downloadHandler.text;
-            Debug.Log("response = " + response);
-            string[] sArray = response.Split('a');
-            GameData.Past = int.Parse(sArray[0]);
-            GameData.DiceNum = int.Parse(sArray[1]);
-            Debug.Log("Past = " + int.Parse(sArray[0]));
-            Debug.Log("DiceNum = " + int.Parse(sArray[1]));
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError ||
+                webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(webRequest.error);
+            }
+            else
+            {
+                string response = webRequest.downloadHandler.text;
+                response = response.Trim();
+                Debug.Log("response = " + response);
+                string[] sArray = response.Split('a');
+                GameData.Past = int.Parse(sArray[0]);
+                GameData.DiceNum = int.Parse(sArray[1]);
+                Debug.Log("Past = " + GameData.Past);
+                Debug.Log("DiceNum = " + GameData.DiceNum);
+            }
         }
     }
 }
